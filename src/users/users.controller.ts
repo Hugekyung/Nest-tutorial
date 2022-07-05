406bdc257d66cc03ba796da4dd8cf272ffb22e7f
haechan <kiki9510@gmail.com>
2022-07-05 18:43:37 +0900

    feat: user.controller에 userService 주입, findAllUsers, createUser 수정

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,30 @@
-import { Body, Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Req, Res } from '@nestjs/common';
 import { Request } from 'express';
 import { UserDto } from './dto/credentialDto';
 import { UserListPaging } from './dto/userListPaging';
+import { User } from './types/user.interface';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
   @Get()
   usersList(@Req() req: Request, @Query() query: UserListPaging): string {
     console.log(req.body);
     return `User Page! page : ${query.page}, limit : ${query.limit}`;
   }
 
+  @Get()
+  findAllUsers(): User[] {
+    const userList = this.userService.findAllUsers();
+    return userList;
+  }
+
   @Post()
   createUser(@Body() createUserDto: UserDto): string {
-    console.log(createUserDto);
-    return `Created user Successfully! Username : ${createUserDto.username}`;
+    this.userService.createUser(createUserDto);
+    return `Create New User! : ${createUserDto.username}`;
   }
 
   @Get(':id')
