a663b47e63a04ee00bca2a4b7f65966a89509566
haechan <kiki9510@gmail.com>
2022-07-14 00:48:22 +0900

    chore: 불필요한 app.controller, service 제거 & 환경변수 설정

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || process.env.ALTER_PORT;
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
