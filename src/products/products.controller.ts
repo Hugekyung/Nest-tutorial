da7db48e08c54f854ae39482eb1e4f82b38b0b1a
haechan <kiki9510@gmail.com>
2022-07-14 00:25:07 +0900

    feat: products controller getProduct 추가

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { IQueryString } from './types/products.interface';
 
 @Controller('products')
@@ -6,5 +6,10 @@ export class ProductsController {
   @Get()
   productList(@Query() queryString?: IQueryString) {
     return `GET category: ${queryString.category} productList.`;
+  }
+
+  @Get(':pid')
+  getProduct(@Param('pid') pid: string) {
+    return `GET Product Details that productId: ${pid}`;
   }
 }
