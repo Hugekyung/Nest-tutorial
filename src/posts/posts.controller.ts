e69afd7311ad1fe71a17aadcb8a0bc99cad613d4
haechan <kiki9510@gmail.com>
2022-08-09 00:12:53 +0900

    feat: posts.controller postList 생성

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  postsList() {
+    const posts = this.postsService.getAllPosts();
+    return posts;
+  }
+}
