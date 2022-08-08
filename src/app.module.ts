2ecf9cb2b970ba816a85467713fe62fb30975dbe
haechan <kiki9510@gmail.com>
2022-08-08 23:40:15 +0900

    feat: posts module 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), UsersModule, EmailModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), UsersModule, EmailModule, PostsModule],
   providers: [],
 })
 export class AppModule {}
