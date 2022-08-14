70b39dea04936d29f2b5475d45a40bf220110989
haechan <kiki9510@gmail.com>
2022-08-14 19:11:54 +0900

    feat: posts.repository findAll 작성

--- src/repository/posts.repository.ts
+++ src/repository/posts.repository.ts
@@ -2,4 +2,8 @@ import { Post } from '../posts/post.entity';
 import { EntityRepository, Repository } from 'typeorm';
 
 @EntityRepository(Post)
-export class PostRepository extends Repository<Post> {}
+export class PostRepository extends Repository<Post> {
+  async findAll() {
+    return await this.find();
+  }
+}
