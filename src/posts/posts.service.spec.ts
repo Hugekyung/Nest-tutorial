413eba4a8d0a3e7d914db177c59d7524dcedb829
haechan <kiki9510@gmail.com>
2022-08-18 00:25:44 +0900

    feat: getAllPosts 테스트 코드 작성중

--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -1,21 +1,34 @@
 import { HttpStatus } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreatePostDto } from './dto/createPostDto';
 import { Post } from './post.entity';
 import { PostsService } from './posts.service';
 
 describe('PostsService', () => {
   let service: PostsService;
+  let postDB: Post[];
+
+  //   class MockPostRepository {}
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        //   { provide: getRepositoryToken(Post), useClass: MockUserRepository },
+        //   { provide: getRepositoryToken(Post), useClass: MockPostRepository },
       ],
     }).compile();
 
     service = module.get<PostsService>(PostsService);
+    postDB = [];
+    // service.createPost(createPostDto);
+  });
+
+  describe('gertAllPosts TEST', () => {
+    const postData: CreatePostDto = {
+      title: 'test-post',
+      description: 'test-description',
+      userId: 1,
+    };
+    // service.createPost = jest.fn((postData) => postDB.push(postData)).mockResolvedValue()
   });
 });
-
-describe('gertAllPosts TEST', () => {});
