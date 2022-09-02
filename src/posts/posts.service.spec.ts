b0de7c3a9e8dde3fd484051b6c16b0867fece5b6
haechan <kiki9510@gmail.com>
2022-09-03 01:51:05 +0900

    Added codes for testing PostsService

--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -1,35 +1,61 @@
 import { HttpStatus } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { PostRepository } from '../repository/posts.repository';
+import { Connection, QueryRunner } from 'typeorm';
 import { CreatePostDto } from './dto/createPostDto';
 import { Post } from './post.entity';
 import { PostsService } from './posts.service';
+import { UserRepository } from '../repository/users.repository';
 
 describe('PostsService', () => {
   let service: PostsService;
   let postDB: Post[];
+  const postData: CreatePostDto = {
+    title: 'test-post',
+    description: 'test-description',
+    userId: 1,
+  };
 
-  //   class MockPostRepository {}
+  const queryRunner = {
+    manager: {},
+  } as QueryRunner;
+
+  class MockConnection {
+    // 2
+    createQueryRunner(mode?: 'master' | 'slave'): QueryRunner {
+      return queryRunner;
+    }
+  }
+
+  class MockPostRepository {
+    postDB = [postData];
+    findAll = () => postDB;
+  }
+
+  class MockUserRepository {}
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        //   { provide: getRepositoryToken(Post), useClass: MockPostRepository },
+        PostsService,
+        { provide: getRepositoryToken(PostRepository), useClass: MockPostRepository },
+        { provide: getRepositoryToken(UserRepository), useClass: MockUserRepository },
+        {
+          provide: Connection, // 이 부분 맞는지 확인 필요
+          useClass: MockConnection,
+        },
       ],
     }).compile();
 
     service = module.get<PostsService>(PostsService);
-    postDB = [];
     // service.createPost(createPostDto);
   });
 
-  describe('gertAllPosts TEST', () => {
-    const postData: CreatePostDto = {
-      title: 'test-post',
-      description: 'test-description',
-      userId: 1,
-    };
-    console.log(postDB);
-    expect(postDB).toEqual([]);
-    // service.createPost = jest.fn((postData) => postDB.push(postData)).mockResolvedValue()
+  describe('getAllPosts TEST', () => {
+    it('getAllPosts 함수를 호출하면 전체 Post를 조회한다.', async () => {
+      const res = await service.getAllPosts();
+      expect(res).toEqual([]);
+    });
   });
 });
