279cf32f23bc69e19b00091ce8863b7850ba2e70
haechan <kiki9510@gmail.com>
2022-09-20 00:59:50 +0900

    Added webpack for Hot Module-Reloading

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import * as path from 'path';
 
+declare const module: any;
+
 dotenv.config({
   path: path.resolve(process.env.NODE_ENV === 'prod' ? '.pord.env' : '.dev.env'),
 });
@@ -18,6 +20,12 @@ async function bootstrap() {
     }),
   );
   await app.listen(PORT);
+
+  // Hot Module-Reloading
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
   console.log(`Started App Successfully with ${PORT} ...`);
 }
 bootstrap();
