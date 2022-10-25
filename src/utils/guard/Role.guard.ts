b0fd4ac20c7c07a721cc965725c56b1e0e74f5b6
haechan <kiki9510@gmail.com>
2022-10-26 01:11:14 +0900

    guard 예제 코드 추가

---
+++ src/utils/guard/Role.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    if (!roles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    // * 적용하고자 하는 로직을 담은 함수
+    // return matchRoles(roles, user.roles);
+  }
+}
